c5b515c17adbe974afa11c71250a8772c7487cb2
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-11-19 14:34:17 -0700

    added layouts, header, footer, and login page, no functionality

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-Login
+<? require_once 'layouts/header.php' ?>
+
+	<form action="index.php" method="POST">
+		<fieldset>
+			<legend>Login</legend>
+			<p><label for="email">E-mail</label><input type="text" name="email" /></p>
+			<p><label for="password">Password</label><input type="password" /></p>
+			<p><input type="submit" value="Login" /></p>
+		</fieldset>
+	</form>
+
+<? require_once 'layouts/footer.php' ?>
---
+++ layouts/header.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<title>Y U MEME ME???</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header>
+		<h1>Y U MEME ME???</h1>
+	</header>
