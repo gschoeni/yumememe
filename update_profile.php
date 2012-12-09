988731ac36af944d3e1f7757cede800132cafb0d
Greg Schoeninger <greg.schoeninger@gmail.com>
2012-12-09 12:00:09 -0700

    removed moved files

--- update_profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once('php_helpers/init.php');
-	check_logged_in();
-	upload_photo_with_dir_and_id('uploads/profile_pictures/', $_SESSION['user_id']);
-	header('Location: update_profile_page.php');
-?>
--- upload_meme.php
+++
@@ -1,6 +0,0 @@
-<?php
-	require_once('php_helpers/init.php');
-	check_logged_in();
-	upload_photo_with_dir_and_id('uploads/memes/', DbHelper::insert_meme($_POST['img_title'], $_SESSION['user_id']));
-	header('Location: profile_page.php');
-?>
